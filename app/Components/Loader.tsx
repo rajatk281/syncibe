1aff9fa4240a3a3870e6e58d82074da24fe68d1a
NNobittaa <rajat28012006@gmail.com>
2026-04-12 23:16:50 +0530

    fix check

--- app/Components/Loader.tsx
+++ app/Components/Loader.tsx
@@ -8,7 +8,7 @@ const Loader = () => {
       clipPath: "inset(0 0% 0 0)", 
       transition: {
         delay: 0.2, 
-        duration: 3,
+        duration: 3.2,
         ease: "easeInOut"
       }
     }
@@ -17,7 +17,7 @@ const Loader = () => {
   return (<>
     <motion.div initial={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 0.6 }} className='relative flex justify-center items-center h-screen overflow-hidden bg-gradient-to-r from-purple-600 to-pink-300'>
+          transition={{ duration: 1 }} className='relative flex justify-center items-center h-screen overflow-hidden bg-gradient-to-r from-purple-600 to-pink-300'>
 
       <div className="absolute inset-0 z-10 pointer-events-none">
         <video 
